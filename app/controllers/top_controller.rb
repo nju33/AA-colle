04dd42d658ba369e3039d1a305896732ea9023db
totora0155 <sski0155@gmail.com>
2012-11-05 14:32:28 -0800

    jsを下に,1ページの読み込みを１３に

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,7 +2,7 @@ class TopController < ApplicationController
   skip_before_filter :authorize
 
   def index
-    @asciiArts = Aa.paginate page: params[:page], order: 'created_at desc', per_page: 8
+    @asciiArts = Aa.paginate page: params[:page], order: 'created_at desc', per_page: 13
     @popularTags = Tag.select('tag, COUNT(*)').group('tag').having('COUNT(*) >= 10')
     @randomTags = Tag.select('tag, COUNT(*)').group('tag').order('RANDOM()').limit(10)
 
