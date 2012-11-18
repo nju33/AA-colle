44d054be148018efb7b618af0972936391650803
totora0155 <sski0155@gmail.com>
2012-11-18 18:50:45 +0900

    Twitterログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,26 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :password_digest, :email
+  attr_accessible :name, :password_digest, :email, :provider, :uid
   attr_accessible :password, :password_confirmation
 
   has_many :aas
 
   validates :name, presence: true, uniqueness: true
   has_secure_password
+
+  def self.create_with_omniauth(auth)
+    create! do |user| 
+      user.provider = auth["provider"] 
+      user.uid = auth["uid"] 
+      user.name = auth.info.name
+      user.password_digest = Product_pass.rand_password
+      #omniuser.screen_name = auth.info.nickname
+    end  
+  end 
+
+  class Product_pass
+    def self.rand_password(length = 8)
+      list = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
+      list.sample(8).join
+    end
+  end
 end
