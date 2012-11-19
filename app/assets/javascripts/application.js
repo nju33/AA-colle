dc387fb1ea7c2446937d817433b15ded692f1f3e
totora0155 <sski0155@gmail.com>
2012-11-19 19:36:45 +0900

    jsの読み込み方変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+// require_tree 
+//= require equalize.min
+//= require jquery.zclip.min
+//= require aa_copy
+//= require contents_equarize
+//= require scroll_button
