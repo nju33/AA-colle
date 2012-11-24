f1d02a5c5635cef348ec684cf19d14af94abd214
totora0155 <sski0155@gmail.com>
2012-11-24 22:44:01 +0900

    sanitize: fauls

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,21 @@
 module ApplicationHelper
 
-  def aa_format(aa, html_options = {}, options = {})
-    aa = '' if aa.nil?
-    aa = aa.dup
-    wrap_tag = tag('p', html_options, true)
+  #def aa_format(aa, html_options = {}, options = {})
+    # 正規表現がわかりましぇん
+    #aa = '' if aa.nil?
+    #aa = aa.dup
+    #wrap_tag = tag('p', html_options, true)
     # aa = sanitize(aa, tags: %w(br)) unless options[:sanitize] == false
-    aa = aa.to_str
+    #aa = aa.to_str
     #aa.gsub!(/\\r\\n?/, "\n")
-    aa.gsub!(/\\r\\n+/, "\1<br>")
-    aa.gsub!(/([^\n]\n)(?=[^\n])/, "\1<br>")
-    aa.insert 0, wrap_tag
-    aa.html_safe.safe_concat("</p>")
-  end
+    #aa.gsub!(/(^").+/, "\1<br>")
+    #aa.gsub!(/("+)(.?)/, "\1[]")
+    # aa.gsub!(/\[^\s]/)
+    #aa.gsub!(%r(\n\n+), "<br>")
+    #aa.gsub!(%r(([^\n]\n)(?=[^\n])), "\1<br>")
+    #aa.gsub!(/(<br>)(+?)/, "\1<br>")
+    #aa.insert 0, wrap_tag
+    #aa.html_safe.safe_concat("</p>")
+  #end
 
 end
